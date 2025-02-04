5e3250b93528f5dbb11ccd342cefc7f0dae607b5
vietanhlee <levietanhtrump@gmail.com>
2025-02-04 23:14:39 +0700

    update end

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import threading
 
 
 # Load model tốt nhất đã được train
-model = YOLO(r'\Identify Fire\model.pt')
+model = YOLO(r'model.pt')
 
 # Khởi tạo camera, 0 mặc định là camera ở local
-cam = cv2.VideoCapture(r'\Identify Fire\test.mp4')
+cam = cv2.VideoCapture(r'test.mp4')
 
 api_key = '7278180996:AAF3zjRmDm2tpTYzl5W1rRXMfTBkt47xWBA'
 id = '5510302349'
@@ -22,40 +22,37 @@ def send_telegram_message():
 while True:
     # Đọc ảnh
     check, cap = cam.read()
-    cap = cv2.flip(cap, 1)
+    # cap = cv2.flip(cap, 1)
     if not check:
         print("Không thể đọc từ camera.")
         break
     
     # Dự đoán 
     res = model.predict(source= cap, conf = 0.4, verbose= False) # verbose = False để tắt các dòng thông báo thừa
-    res = res[0] 
-    cls = res.boxes.cls
+
     # Lấy ra độ tin cậy của từng box
-    conf = res.boxes.conf
+    conf = res[0].boxes.conf
     # Lấy ra data về các box
-    boxes = res.boxes.xyxy
+    boxes = res[0].boxes.xyxy
 
     # Vẽ và hiển thị thông qua cv2
     for i in range(len(boxes)):
-        if(cls[i] == 0):
-            # Ép kiểu về số nguyên do yêu cầu thông số của cv2 qui định
-            x1, y1, x2, y2 = map(int, boxes[i]) # Lấy tọa độ box
-            # Vẽ box
-            cv2.rectangle(cap, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            # Điền độ tin cậy của box
-            cv2.putText(cap, f'{round(float(conf[i]), 2)}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            # vẽ tâm box
-            x, y = (x1 + x2) //2, (y1 + y2) // 2 
-            color = (0, 255, 0)             
-            cv2.circle(cap, (x, y), radius=3, color=color, thickness=-1)  # thickness=-1 để tô đầy hình tròn
+        # Ép kiểu về số nguyên do yêu cầu thông số của cv2 qui định
+        x1, y1, x2, y2 = map(int, boxes[i]) # Lấy tọa độ box
+        # Vẽ box
+        cv2.rectangle(cap, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        # Điền độ tin cậy của box
+        cv2.putText(cap, f'{round(float(conf[i]), 2)}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        # vẽ tâm box
+        x, y = (x1 + x2) //2, (y1 + y2) // 2 
+        color = (0, 255, 0)             
+        cv2.circle(cap, (x, y), radius=3, color=color, thickness=-1)  # thickness=-1 để tô đầy hình tròn
     
     # Thông báo về số ngọn lửa được tìm thấy
     fire = len(boxes)
     if(fire != 0):
         # Ghi lên màn
         cv2.putText(cap, f'{fire} fire detected', (30, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
-        # Cảnh báo cháy qua telegram
     else:
         cv2.putText(cap, f'No fire visible in the observation area', (30, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         if(api_key != None):
